1173d3c3e3b8595710524b827e49ce7e93d4f74a
Rodrigo Aparicio <101743518+Zuack55@users.noreply.github.com>
2023-02-24 15:10:15 -0600

    Url change
    create file urls for add conts
    
    Co-Authored-By: Leonardo-Aguirre-Ponce <72171210+Leonardo-Aguirre-Ponce@users.noreply.github.com>
    Co-Authored-By: victordoom <23017227+victordoom@users.noreply.github.com>
    Co-Authored-By: Gaddiel Patiño <50815656+GaddPat0609@users.noreply.github.com>

--- src/views/AddRecord.jsx
+++ src/views/AddRecord.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 import Headimg from "../components/Headimg";
+import { ImgHead } from "../themes/Urls";
+
 
 export class AddRecord extends Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View>
         <Headimg
           Text="Registro de Daños"
-          ImgUrl="https://img.freepik.com/foto-gratis/fondo-blanco-pared-piedra-pintada_1252-1057.jpg?w=1380&t=st=1677195424~exp=1677196024~hmac=b0d850dcbcb3aecdf0279c8eed2c728ea9bd6eb61a3091876fbf9adae18534e6"
+          ImgUrl={ImgHead}
         />
       </View>
     );
