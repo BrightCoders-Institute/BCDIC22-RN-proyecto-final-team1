a794c2b9724ad6852379231c25b293566c5d1ba3
Rodrigo Aparicio <101743518+Zuack55@users.noreply.github.com>
2023-03-01 16:22:58 -0600

    Feature#13-Screen-details
    create the detail screen layout
    
    Co-Authored-By: Leonardo-Aguirre-Ponce <72171210+Leonardo-Aguirre-Ponce@users.noreply.github.com>
    Co-Authored-By: Gaddiel Patiño <50815656+gadd0609@users.noreply.github.com>
    Co-Authored-By: victordoom <23017227+victordoom@users.noreply.github.com>

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import StartupScreen from "./src/views/StartupScreen";
 import SignUpScreen from "./src/views/SignUpScreen";
 import AddRecord from "./src/views/AddRecord";
 import MyRecords from "./src/views/MyRecords";
+import Details from "./src/views/Details";
 
 export default function App() {
-  return <MyRecords />;
+  return <Details />;
 }
---
+++ src/themes/DetailStyle.jsx
@@ -0,0 +1,76 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "./colors";
+import { FWEIGHT } from "./fweight";
+
+export const DetailStyle = StyleSheet.create({
+  containerDetails: {
+    backgroundColor: COLORS.DARK_BLUE,
+    width: "100%",
+    height: "100%",
+  },
+  customMap: {
+    height: 300,
+    width: "100%",
+    backgroundColor: COLORS.DARK_BLUE,
+  },
+  containerArrow: { position: "absolute", top: 40, zIndex: 100 },
+  containerInfo: { alignSelf: "center" },
+  containerDirection: { flexDirection: "row", marginTop: 20 },
+  textDetail: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: FWEIGHT.STRONG,
+    marginTop: 10,
+  },
+  dividerPhoto: {
+    backgroundColor: COLORS.WHITE,
+    height: 1,
+    width: "67%",
+    marginLeft: 10,
+    marginTop: 27,
+  },
+  containerImg: { width: 300, height: 200, marginLeft: 30, marginTop: 20, marginBottom: 20},
+  img: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  containerDesc: { flexDirection: "row", marginTop: 27, marginTop: 20, marginBottom: 20 },
+  textDesc: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: FWEIGHT.STRONG,
+    marginTop: 10,
+  },
+  dividerDesc: {
+    backgroundColor: COLORS.WHITE,
+    height: 1,
+    width: "63%",
+    marginLeft: 10,
+    marginTop: 27,
+  },
+  containerDescDetails: { width: 300, height: 200, marginLeft: 30, fontSize: 25 },
+  textDescDetails: { color: "white", marginTop: 10, fontSize: 17, textAlign: "justify" },
+  containerDate: { flexDirection: "row"},
+  textDate: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 5,
+  },
+  dividerDate: {
+    backgroundColor: COLORS.WHITE,
+    height: 1,
+    width: "76%",
+    marginLeft: 10,
+    marginTop: 20,
+  },
+  containerDateDetails: { width: 300, height: 200, marginLeft: 30 },
+  textDateDetails: {
+    color: COLORS.WHITE,
+    marginTop: 20,
+    alignSelf: "center",
+    fontSize: 25,
+  },
+});
