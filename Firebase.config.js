8c3b775f7cd68414751f71c64c9db11cdd897f1d
Gaddiel Patiño <alegaddiel@gmail.com>
2023-03-07 17:52:15 -0600

    Feature#16-Firebase-config
    Firebase configuration
    
    Co-Authored-By: Rodrigo Aparicio <101743518+Zuack55@users.noreply.github.com>
    Co-Authored-By: Leonardo-Aguirre-Ponce <72171210+Leonardo-Aguirre-Ponce@users.noreply.github.com>
    Co-Authored-By: victordoom <23017227+victordoom@users.noreply.github.com>

---
+++ Firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzawW7qnt7M-SjlPr7_Gg22GAhJNz_0wE",
+  authDomain: "damage-control-ba92f.firebaseapp.com",
+  projectId: "damage-control-ba92f",
+  storageBucket: "damage-control-ba92f.appspot.com",
+  messagingSenderId: "127685083270",
+  appId: "1:127685083270:web:58bf57eb6d5dfaf3bc1107",
+};
+
+export const app = initializeApp(firebaseConfig);
