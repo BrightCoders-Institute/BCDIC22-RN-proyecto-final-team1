bb969a6de610d3336b5b780e611f590b30d3cd18
Leonardo-Aguirre-Ponce <laguirre4@ucol.mx>
2023-03-08 21:04:37 -0600

    dotenv implementation
    Added the dotenv module to avoid exposing secret tokens
    
    Co-Authored-By: Gaddiel Patiño <50815656+gadd0609@users.noreply.github.com>
    Co-Authored-By: victordoom <23017227+victordoom@users.noreply.github.com>
    Co-Authored-By: Rodrigo Aparicio <101743518+Zuack55@users.noreply.github.com>

--- Firebase.config.js
+++ Firebase.config.js
@@ -1,12 +1,14 @@
 import { initializeApp } from "firebase/app";
+import { config } from "dotenv";
+config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzawW7qnt7M-SjlPr7_Gg22GAhJNz_0wE",
-  authDomain: "damage-control-ba92f.firebaseapp.com",
-  projectId: "damage-control-ba92f",
-  storageBucket: "damage-control-ba92f.appspot.com",
-  messagingSenderId: "127685083270",
-  appId: "1:127685083270:web:58bf57eb6d5dfaf3bc1107",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
-export const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
